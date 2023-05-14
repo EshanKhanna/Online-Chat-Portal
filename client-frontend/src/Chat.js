3f0eb2d18fea2a1293f4894ac2ccfb23bb496206
EshanKhanna <khannaeshan@yahoo.in>
2023-05-14 19:31:36 +0530

    3rd Commit

--- client-frontend/src/Chat.js
+++ client-frontend/src/Chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import ScrollToBottom from "react-scroll-to-bottom";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -34,22 +34,45 @@ function Chat({ socket, username, room }) {
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-        <div className="chat-body">
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
           {messageList.map((messageContent) => {
-             
-          })}
+            
+            return (
+              <div
+                className="message"
+               
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
                 </div>
-        <div className="chat-footer">
-        <input type="text" 
-        placeholder="Hey..." 
-        onChange={(event) => {
-              setCurrentMessage(event.target.value);
-            }}
-            />
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
         <button onClick={sendMessage}>&#9658;</button>
-        </div>
+      </div>
     </div>
-    )
-        }
+  );
+}
 
-    export default Chat;
+export default Chat;
